7a8ec8e9938fc3cf429b667f6b96de825cde5009
andrelobo <loboandre@hotmail.com>
2020-08-31 20:18:23 -0400

    soundza

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ class CardProfile extends React.Component {constructor(...args) {super(...args);
     {
       index: 3,
       currentTime: '0:00',
-      musicList: [{ name: 'Nice piano and ukulele', author: 'Royalty', img: 'https://www.bensound.com/bensound-img/buddy.jpg', audio: 'https://www.bensound.com/bensound-music/bensound-buddy.mp3', duration: '2:02' },
-      { name: 'Gentle acoustic', author: 'Acoustic', img: 'https://www.bensound.com/bensound-img/sunny.jpg', audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3', duration: '2:20' },
-      { name: 'Gentle acoustic', author: 'Acoustic', img: 'https://www.bensound.com/bensound-img/sunny.jpg', audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3', duration: '2:20' },
+      musicList: [{ name: 'Nice piano and ukulele', author: 'Royalty', img: 'https://www.bensound.com/bensound-img/buddy.jpg', audio: 'https://andrelobo.github.io/soundza/cacetada.mp3', duration: '2:02' },
+      { name: 'Gentle acoustic', author: 'Acoustic', img: 'https://www.bensound.com/bensound-img/sunny.jpg', audio: 'https://andrelobo.github.io/soundza/Tunfs.mp3', duration: '2:20' },
+      { name: 'Gentle acoustic', author: 'Acoustic', img: 'https://www.bensound.com/bensound-img/sunny.jpg', audio: 'https://andrelobo.github.io/soundza/db1.mp3' },
       { name: 'Gentle acoustic', author: 'Acoustic', img: 'https://www.bensound.com/bensound-img/sunny.jpg', audio: 'https://www.bensound.com//bensound-music/bensound-sunny.mp3', duration: '2:20' },
       { name: 'Corporate motivational', author: 'Corporate', img: 'https://www.bensound.com/bensound-img/energy.jpg', audio: 'https://www.bensound.com/bensound-music/bensound-energy.mp3', duration: '2:59' },
       { name: 'Slow cinematic', author: 'Royalty', img: 'https://www.bensound.com/bensound-img/slowmotion.jpg', audio: 'https://www.bensound.com/bensound-music/bensound-slowmotion.mp3', duration: '3:26' }],
